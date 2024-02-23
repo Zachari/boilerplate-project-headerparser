6ca724343c7ae94b1e0f6bd3bdc407dcfde19009
Zachari LaCombe <zac@zachari.io>
2024-02-23 03:40:24 +0000

    Corrected ip to ipaddress

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/hello', function (req, res) {
 
 app.get('/api/whoami', (req, res) => {
   res.json({
-    ip: req.ip,
+    ipaddress: req.ip,
     language: req.headers["accept-language"],
     software: req.headers["user-agent"]
   });
